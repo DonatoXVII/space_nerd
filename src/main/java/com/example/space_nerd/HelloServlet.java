e803c3daba9be37bb6ad032c0dcdc1fa8f6d777c
DonatoXVII <d.folgieri@studenti.unisa.it>
2024-02-19 12:29:33 +0100

    Correzioni di prova SonarCloud, creazione context.xml e mysql-connector

--- src/main/java/com/example/space_nerd/HelloServlet.java
+++ src/main/java/com/example/space_nerd/HelloServlet.java
@@ -9,10 +9,12 @@ import jakarta.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private String message;
 
+    @Override
     public void init() {
         message = "Hello World!";
     }
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
@@ -21,8 +23,5 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
-    }
-
-    public void destroy() {
     }
 }

608eece59c7d3afecba2724b4c6b9b258e9113bc
DonatoXVII <d.folgieri@studenti.unisa.it>
2024-02-20 17:34:13 +0100

    Correzioni SonarCloud

--- src/main/java/com/example/space_nerd/Model/UtenteModel.java
+++ src/main/java/com/example/space_nerd/Model/UtenteModel.java
@@ -92,11 +92,19 @@ public class UtenteModel {
         } catch (SQLException e) {
             logger.log(Level.WARNING, e.getMessage());
         } finally {
-            if (ps != null) {
-                ps.close();
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                logger.log(Level.WARNING, "Errore durante la chiusura del PreparedStatement", e);
             }
-            if (con != null) {
-                con.close();
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                logger.log(Level.WARNING, "Errore durante la chiusura della Connection", e);
             }
         }
     }
@@ -118,14 +126,26 @@ public class UtenteModel {
         } catch (SQLException e) {
             logger.log(Level.WARNING, e.getMessage());
         } finally {
-            if (rs != null) {
-                rs.close();
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                logger.log(Level.WARNING, "Errore durante la chiusura del ResultSet", e);
             }
-            if (ps != null) {
-                ps.close();
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                logger.log(Level.WARNING, "Errore durante la chiusura del PreparedStatement", e);
             }
-            if (con != null) {
-                con.close();
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                logger.log(Level.WARNING, "Errore durante la chiusura della Connection", e);
             }
         }
         return res;
